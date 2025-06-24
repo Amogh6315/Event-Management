d540fd7130492a1244563a800fe1b7a6708601d5
Amogh <amoghgupta6315@gmail.com>
2025-06-24 22:55:12 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center font-bold text-green-800 min-h-screen justify-center">
+      <h1 className="text-4xl mb-2">Welcome Tiya to Amya</h1>
+      <h3 className="text-xl">India's best Event Management company</h3>
     </div>
   );
 }
